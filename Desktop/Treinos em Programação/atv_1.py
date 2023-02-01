eb79206e8c66b31f5da6e5340c20804577b8600c
CostelaGit <84296864+CostelaGit@users.noreply.github.com>
2023-01-31 21:54:14 -0300

    Add files via upload

---
+++ Desktop/Treinos em Programação/atv_1.py
@@ -0,0 +1,12 @@
+print("Luiz Gustavo")
+print("18 anos")
+print("1.75")
+
+
+#2 parte
+
+print("Nome: Luiz gustavo", "\nidade: 18 anos", "\naltura: 1.75")
+
+#3 parte
+
+print('18', '24', '15', sep='-')
